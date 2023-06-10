9532253de4ae0c568e0b14d73030b6e07364a2e2
henav <henav@tuebingen.mpg.de>
2023-06-10 12:10:27 +0200

    Fixed trailing slash issue in input paths. Changed the maximal number of retrieved hits in the BLASTn search

--- find_overlapping_regions.sh
+++ find_overlapping_regions.sh
@@ -11,9 +11,9 @@ minimal_identity="${i:=97}"
 while getopts t:r:o:c:i:l: flag
 do
     case "${flag}" in
-        t) target=${OPTARG};; #metagenome assemlies/genomes - used to create the blast DB
-        r) references=${OPTARG};; # folder with reference genomes 
-        o) output=${OPTARG};; #output and temp folders will be written to this path
+        t) target=${OPTARG%/};; #metagenome assemlies/genomes - used to create the blast DB
+        r) references=${OPTARG%/};; # folder with reference genomes 
+        o) output=${OPTARG%/};; #output and temp folders will be written to this path
         i) minimal_identity=${OPTARG};; #minimal blast identity
         c) minimal_coverage=${OPTARG};; #blast qcov_hsp_perc
         l) length=${OPTARG};; #length of flanking sequences (on the retrieved traget sequence)
@@ -21,8 +21,8 @@ do
 done
 
 output=$(readlink -f "$output") #get the full path of the output folder
-
-
+references="$references/"
+target="$target/"
 # step 0: run the python script first, to find the "central_regions"
 rm -rf "$output"
 mkdir "$output"
@@ -32,35 +32,28 @@ central_regions="$output/central_regions/"
 python central_regions.py $references $central_regions
 
 
-
-# input file naming convention (per-sample assembly files/genome files):
-# sample name should be the first part of the file name, followed by "_" and added information, if wanted
-# The sample name should not include any special characters!!!
-# The file name should not include "/" or ".", other special characters are accepted.  
-
 # step 1: run script to:
 #	a. change sample names (i.e., assemblies/genomes) to Sample.xxx
 #	b. change fasta headers to contig.xxx
 #	c. merge fasta files to one file
 #	d. create a table with old and new names. 
 
-echo "starting merging metagenomes/genome files"
+echo $'starting merging metagnome-assemblies/genome files\n'
 
 combined_output="$output/combined_targets/"
 python old_to_new_names.py $target $combined_output
 
-echo "Merging metagenome/genome files is complete"
+echo $'\nMerging metagnome-assemblies/genome files is complete'
 
 
 # step 2:  Make a blast database, from all the contigs. 
 #          Rerunning this step overrides the previous blast DB - pay attention!
 newdir="$output/blastDB"
-echo "newdir: $newdir"
 rm -rf "$newdir"
 mkdir "$newdir"
 makeblastdb -in  "$combined_output/combined_renamed_genomes.fasta" -out "$newdir/GroupsDB" -title "GroupsDB" -parse_seqids -dbtype nucl
 
-echo "Making blast DB is complete"
+echo $'Making blast DB is complete\n'
 
 # Step 3: #run blast per genome => per region
 #a. make output folder
@@ -72,20 +65,16 @@ rm -rf "$newdir1"
 mkdir "$newdir1"
 
 #run blast 
-#cd "$newdir"
 
 for folder in "$central_regions"/*; do
-	#echo "$folder"
-	#echo "$newdir1/"$(basename "${folder%.*}")""
 	rm -rf "$newdir1/"$(basename "${folder%.*}")""
 	mkdir "$newdir1/"$(basename "${folder%.*}")""
 	for query in "$folder"/*.fasta; do
-	blastn -query "$query" -db "$newdir/GroupsDB" -out "$newdir1/"$(basename "${folder%.*}")"/$(basename "${query%.*}").tab"  -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore sstrand" -perc_identity "$minimal_identity" -qcov_hsp_perc "$minimal_coverage" -num_threads 8;
-	#echo "blastn -query "$query" -db "$newdir/GroupsDB" -out "$newdir1/"$(basename "${folder%.*}")"/$(basename "${query%.*}").tab"  -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore sstrand" -perc_identity "$minimal_identity" -qcov_hsp_perc "$minimal_coverage" -num_threads 8;";
+	blastn -query "$query" -db "$newdir/GroupsDB" -out "$newdir1/"$(basename "${folder%.*}")"/$(basename "${query%.*}").tab"  -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore sstrand"  -max_target_seqs 10000 -perc_identity "$minimal_identity" -qcov_hsp_perc "$minimal_coverage" -num_threads 8;
 	done
 done
 
-echo "BLAST search is complete"
+echo $'BLAST search is complete!\n'
 
 
 
@@ -93,12 +82,11 @@ echo "BLAST search is complete"
 # Step 4: delete blast output files with less than 2 hits
 for folder in "$newdir1/"*; do
 	for seqfolder in "$folder"/*; do
-	#echo "$seqfolder"
 	find "$seqfolder" -type f -exec awk -v x=2 'NR==x{exit 1}' {} \; -exec rm -f {} \;
 done
 done
 
-echo "Deleting low-hit regions is complete"
+echo $'Deleting low-hit regions is complete\n'
 
 
 
@@ -117,13 +105,13 @@ export blastDB="$newdir/GroupsDB"
 
 for folder in "$newdir1"/*; do
 	LogFile="$newdir2"/$(basename "${folder%.*}").log 
-	echo "$LogFile"
+	echo $"Retrieval of genomic regions: log file is $LogFile"
 	mkdir "$newdir2/$(basename "${folder%.*}")"
 	for tabular in "$folder"/*.tab; do 
-#	awk -v "len=$length" -v "outlog=$LogFile" '{if ($13 == "minus") print $2"\t"$10-len"\t"$9+len"\t"$13"\t"; else print $2"\t"$9-len"\t"$10+len"\t"$13}' "$tabular" | xargs -n 4 sh -c 'blastdbcmd -db "$blastDB" -entry "$0" -range "$1"-"$2" -strand "$3"  -outfmt %f -logfile outlog;'  >  "$newdir2"/$(basename "${folder%.*}")/$(basename "${tabular%.*}").fasta;
 	awk -v "len=$length" -v "outlog=$LogFile" '{if ($13 == "minus") print $2"\t"$10-len"\t"$9+len"\t"$13"\t"; else print $2"\t"$9-len"\t"$10+len"\t"$13}' "$tabular" | xargs -n 4 sh -c 'blastdbcmd -db "$blastDB" -entry "$0" -range "$1"-"$2" -strand "$3"  -outfmt %f -logfile outlog;'  >  "$newdir2"/$(basename "${folder%.*}")/$(basename "${tabular%.*}").fasta;
-
 	done
 done 
 
-echo "Retrieval of flanking regions is complete"
+echo $'Retrieval of flanking regions is complete!\n'
+
+echo $'First step is complete!\n'
